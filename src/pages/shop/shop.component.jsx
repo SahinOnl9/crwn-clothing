fe23bc4e9f1fde72055b4bd63486376baeb577c3
sahin <sksahin53@gmail.com>
2020-12-18 22:55:21 +0530

    added in shop page: collection preview and collection item component

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import SHOP_DATA from "./shop.data";
+
+export default class ShopPage extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otehrCollectionProps }) => (
+          <CollectionPreview key={id} {...otehrCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
